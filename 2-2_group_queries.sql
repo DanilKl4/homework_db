bd0225acf9174b9a7259bbcbff5ee9eae1078599
Данил Клочков <danil.klochkov.92@bk.ru>
2022-02-18 02:11:06 +0300

    the second task (queries with GROUP BY)

---
+++ 2-2_group_queries.sql
@@ -0,0 +1,69 @@
+-- 1
+SELECT n_group, COUNT(n_group)
+FROM student
+GROUP BY n_group;
+
+-- 2
+SELECT n_group, MAX(score) AS max_score
+FROM student
+GROUP BY n_group
+ORDER BY max_score DESC;
+
+-- 3
+SELECT surname, COUNT(surname)
+FROM student
+GROUP BY surname;
+
+-- 4
+SELECT date_birth, COUNT(id)
+FROM student
+GROUP BY date_birth
+HAVING date_birth IS NOT NULL;
+
+-- 5
+SELECT n_group, AVG(score)
+FROM student 
+GROUP BY n_group
+ORDER BY n_group;
+
+-- 6
+SELECT n_group
+FROM
+	student
+WHERE score IN 
+	(SELECT MAX(score)
+ 	FROM student)
+LIMIT 1;
+
+-- 7
+SELECT n_group, AVG(score) as avg_score
+FROM
+	student
+GROUP BY n_group
+HAVING AVG(score) <= 3.5
+ORDER BY avg_score;
+
+-- 8
+SELECT n_group, AVG(score) as avg_score, MAX(score) as max_score, MIN(score) as min_score
+FROM
+	student
+GROUP BY n_group;
+
+-- 9 Студент с max баллом в 5555
+SELECT *
+FROM student
+WHERE (n_group, score) IN
+	(SELECT n_group, MAX(score)
+	FROM
+		student
+	GROUP BY n_group
+	HAVING n_group = 5555);
+
+-- 10
+SELECT *
+FROM student
+WHERE (n_group, score) IN
+	(SELECT n_group, MAX(score)
+	FROM
+		student
+	GROUP BY n_group);
