6f721682b12c72404de358ab3bac1ed9042c3dd2
zephir <72443284+DanilKlochkov@users.noreply.github.com>
2022-05-29 23:33:05 +0300

    update

--- 2-3_multi-table_queries.sql
+++ 2-3_multi-table_queries.sql
@@ -205,3 +205,124 @@ FROM student s
 ORDER BY s.score DESC;
 
 -- 22
+CREATE OR REPLACE VIEW Students_V1 AS
+SELECT ph.course, ph.hobby
+FROM (SELECT s.n_group / 1000 course, h.name hobby, COUNT(h.name) h_count
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+GROUP BY h.name, s.n_group / 1000
+ORDER BY h_count DESC) ph
+
+-- 23
+CREATE OR REPLACE VIEW Students_V1 AS
+SELECT h.name hobby, COUNT(h.name) h_count, h.risk
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+WHERE s.n_group / 1000 = 2
+GROUP BY h.name, h.risk
+ORDER BY h_count DESC, h.risk DESC
+LIMIT 1;
+
+-- 24
+CREATE OR REPLACE VIEW Students_V1 AS
+SELECT s.n_group / 1000 course, COUNT(s.id) count_all
+FROM student s
+RIGHT JOIN (
+	SELECT s.n_group / 1000 c, COUNT(s.id) count_otl
+	FROM student s
+	WHERE s.score >= 4.5
+	GROUP BY s.n_group / 1000
+) otl on otl.c = s.n_group / 1000
+GROUP BY s.n_group / 1000
+
+-- 25
+CREATE OR REPLACE VIEW popular_hobby AS
+SELECT h.name hobby, COUNT(*)
+FROM hobby h
+LEFT JOIN student_hobby sh ON h.id = sh.hobby_id
+GROUP BY h.name
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+-- 26
+CREATE OR REPLACE VIEW update_view AS
+SELECT id, name, surname, n_group 
+FROM student
+WITH CHECK OPTION;
+
+-- 27
+SELECT left(s.name, 1), MAX(s.score), ROUND(AVG(s.score),2), MIN(s.score)
+FROM student s
+WHERE s.score > 3.6
+GROUP BY left(s.name, 1)
+ORDER BY left(s.name, 1)
+
+-- 28
+SELECT s.n_group / 1000 course, s.surname, MAX(s.score), MIN(s.score)
+FROM student s
+GROUP BY s.n_group / 1000, s.surname
+
+-- 29
+SELECT EXTRACT(YEAR FROM date_birth), COUNT(h.id)
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+WHERE h.id IS NOT NULL
+GROUP BY EXTRACT(YEAR FROM date_birth)
+ORDER BY EXTRACT(YEAR FROM date_birth);
+
+-- 30
+SELECT left(s.name, 1), MAX(h.risk), MIN(h.risk)
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+GROUP BY left(s.name, 1)
+
+-- 31
+SELECT EXTRACT(MONTH FROM s.date_birth), ROUND(AVG(s.score), 2) 
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+WHERE h.name = 'Футбол'
+GROUP BY EXTRACT(MONTH FROM s.date_birth)
+
+-- 32
+SELECT DISTINCT s.name Имя, s.surname Фамилия, s.n_group Группа
+FROM student_hobby sh
+INNER JOIN student s ON s.id = sh.student_id
+INNER JOIN hobby h ON h.id = sh.hobby_id
+
+-- 33
+SELECT 
+CASE WHEN strpos(surname, 'ов') != 0 THEN strpos(surname, 'ов')::VARCHAR(3)
+	 ELSE 'не найдено'
+END
+FROM student;
+
+-- 34
+SELECT rpad(surname, 10, '#') FROM student;
+
+-- 35
+SELECT replace(surname, '#', '') FROM student;
+
+-- 36
+SELECT DATE_TRUNC('month', '2018-04-01'::timestamp) + '1 MONTH'::INTERVAL - DATE_TRUNC('month', '2018-04-01'::timestamptz);
+
+-- 38
+SELECT 
+	EXTRACT(CENTURY FROM now()),
+	EXTRACT(WEEK FROM now()),
+	EXTRACT(DOY FROM now());
+
+-- 39
+SELECT DISTINCT s.name, s.surname, h.name,
+CASE
+	WHEN sh.date_finish IS NULL THEN 'занимается'
+	ELSE 'закончил'
+END 
+FROM student_hobby sh
+INNER JOIN student s ON sh.student_id = s.id
+INNER JOIN hobby h ON h.id = sh.hobby_id;22
+
